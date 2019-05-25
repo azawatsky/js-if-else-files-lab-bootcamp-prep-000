571a33204ddfc84c5647fb837f2f31d884b718da
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-05-25 03:06:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-var name = "Joe";
-var height = "74";
+var name = "Joe"
+var height = "74"
 
-if (name === "AZ") {
-  console.log("The name is correct");
-} else {
-  console.log("Expected the name to be AZ");
-  var name = "Joey";
-  console.log(name);
-}
-
-if (name === "AZ") {
-  console.log("The name is correct");
-} else {
-  console.log("Expected the name to be AZ");
-  var name = "AZ";
-  console.log(name);
-}
-
-if (name === "AZ") {
-  console.log("The name is correct");
-} else {
-  console.log("Expected the name to be AZ");
-  var name = "AZ";
-  console.log(name);
-}
 // Don't worry about this
 module.exports = { name, height
-};
+}
